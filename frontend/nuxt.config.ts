dda85982067b8e5a1e01b22c25cf298e7d5d9709
prokelly <anyeprincekelly816@gmail.com>
2026-08-01 11:24:38 -0500

    deploy

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -15,6 +15,7 @@ export default defineNuxtConfig({
         },
       ],
       link: [
+        { rel: "icon", type: "image/png", href: "/cityos.png" },
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "" },
         {
